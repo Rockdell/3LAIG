18a934ded8d8b0e216dd641c0f34722f2ca17d7b
Miguel Teixeira <miguelalexbt@outlook.com>
2019-01-06 20:45:43 +0000

    Half way done on the new chair

--- game/view/MyBoardView.js
+++ game/view/MyBoardView.js
@@ -32,11 +32,7 @@ class MyBoardView extends CGFobject {
             // this.coffeeMug.display();            
             
             this.scene.translate(0.5, 0, 0.5);
-
-
             this.scene.rotate(-Math.PI / 2.0, 1, 0, 0);
-
-          
 
             this.scene.pushMatrix();
                 this.supportAppearance.apply();
